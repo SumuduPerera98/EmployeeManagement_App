15cc3b21dae89b59513c1eac42a24696ad5e7ce0
SumuduPerera98 <sharshiperera20@gmail.com>
2022-08-01 10:59:05 +0530

    employee list

---
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class ListEmployeeComponent extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            employees: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className='text-center'>Employee List</h2>
+                <div className='row'>
+                    <table className='table table-bordered table-striped'>
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email ID</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {
+                                this.state.employees.map(
+                                    employee =>
+                                    <tr key={employee.id}>
+                                        <td>{employee.firstName}</td>
+                                        <td>{employee.lastName}</td>
+                                        <td>{employee.emailId}</td>
+
+                                    </tr>
+                                )
+                            }
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ListEmployeeComponent;
